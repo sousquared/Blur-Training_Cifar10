ee92feea60b167354baf9ab50a8992dbea5a2d51
root <sou.yoshihara.72e@gmail.com>
2020-02-10 16:48:24 +0900

    change outputs

--- blurnet/blur-half_train.py
+++ blurnet/blur-half_train.py
@@ -10,8 +10,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 
-from blurnet.utils import dataloader, GaussianBlur_images
-from blurnet.models import AlexNetCifar10
+from utils import dataloader, GaussianBlur_images, save_checkpoint
+from models import AlexNetCifar10
 
 # Training settings
 parser = argparse.ArgumentParser()
@@ -35,165 +35,140 @@ parser.add_argument('--lr', type=float, default=0.01,
 parser.add_argument('--weight-decay', '-w', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
 
-args = parser.parse_args()
-if args.exp_name == '':
-    print('ERROR: USE \'--exp-name\' or \'-n\' option to define this experiment\'s name.')
-    sys.exit()
+def main():
+    args = parser.parse_args()
+    if args.exp_name == '':
+        print('ERROR: USE \'--exp-name\' or \'-n\' option to define this experiment\'s name.')
+        sys.exit()
 
-# directories settings
-os.makedirs('../logs/results', exist_ok=True)
-os.makedirs('../logs/params', exist_ok=True)
+    # directories settings
+    os.makedirs('../logs/outputs', exist_ok=True)
+    os.makedirs('../logs/models/{}'.format(args.exp_name), exist_ok=True)
 
-RESULT_PATH = '../logs/results/{}.txt'.format(args.exp_name)
-MODEL_PATH = '../logs/params/{}.pth'.format(args.exp_name)
+    OUTPUT_PATH = '../logs/outputs/{}.log'.format(args.exp_name)
+    MODEL_PATH = '../logs/models/{}/'.format(args.exp_name)
 
 
-if os.path.exists(RESULT_PATH):
-    print('ERROR: This \'--exp-name\' is already used. Use another name for this experiment.')
-    sys.exit()
+    if os.path.exists(RESULT_PATH):
+        print('ERROR: This \'--exp-name\' is already used. Use another name for this experiment.')
+        sys.exit()
 
-# recording outputs
-sys.stdout = open(RESULT_PATH, 'w')
-sys.stderr = open(RESULT_PATH, 'a')
+    # recording outputs
+    sys.stdout = open(OUTPUT_PATH, 'w')
+    sys.stderr = open(OUTPUT_PATH, 'a')
 
-# tensorboardX
-writer = SummaryWriter(log_dir='../logs/tb/{}'.format(args.exp_name))
- 
-# cuda settings
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-device = torch.device("cuda:0" if args.cuda else "cpu")
-print('device: {}'.format(device))
+    # tensorboardX
+    writer = SummaryWriter(log_dir='../logs/tb/{}'.format(args.exp_name))
 
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+    # cuda settings
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda:0" if args.cuda else "cpu")
+    print('device: {}'.format(device))
 
-# data settings
-trainloader, testloader, classes = dataloader(batch_size=args.batch_size)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
 
-# Model, Criterion, Optimizer
-net = AlexNetCifar10().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+    # data settings
+    trainloader, testloader, _ = dataloader(batch_size=args.batch_size)
 
-# print settings
-print('='*5 + ' settings ' + '='*5)
-print('TRAINING MODE: {}'.format('Blur' if not args.normal else 'Normal'))
-print('### NO BLUR FROM EPOCH 25 ###')
-if not args.normal:
-    print('Kernel-size: {}'.format(tuple(args.kernel_size)))
-    print('Sigma: {}'.format(args.sigma))
-print('Random seed: {}'.format(args.seed))
-print('Epochs: {}'.format(args.epochs))
-print('Learning rater: {}'.format(args.lr))
-print('Weight_decay: {}'.format(args.weight_decay))
-print()
-print(net)
-print('='*20)
-print()
+    # Model, Criterion, Optimizer
+    net = AlexNetCifar10().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
 
-# training
-num_trainbatch = -(-len(trainloader.dataset) / trainloader.batch_size) # e.g. 50000 / 4 (cifar10)
-num_testbatch = -(-len(testloader.dataset) / testloader.batch_size)  # e.g. 10000 / 4 (cifar10)
-print('Start Training...')
-train_time = time.time()
-for epoch in range(args.epochs):  # loop over the dataset multiple times
-    train_loss, train_acc, val_loss, val_acc = 0, 0, 0, 0
+    # print settings
+    print('='*5 + ' settings ' + '='*5)
+    print('TRAINING MODE: {}'.format('Blur' if not args.normal else 'Normal'))
+    print('### NO BLUR FROM EPOCH 25 ###')
+    if not args.normal:
+        print('Kernel-size: {}'.format(tuple(args.kernel_size)))
+        print('Sigma: {}'.format(args.sigma))
+    print('Random seed: {}'.format(args.seed))
+    print('Epochs: {}'.format(args.epochs))
+    print('Learning rater: {}'.format(args.lr))
+    print('Weight_decay: {}'.format(args.weight_decay))
+    print()
+    print(net)
+    print('='*20)
+    print()
 
-    # ===== train mode =====
-    net.train()
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data[0].to(device), data[1].to(device)
+    # training
+    num_trainbatch = -(-len(trainloader.dataset) / trainloader.batch_size) # e.g. 50000 / 4 (cifar10)
+    num_testbatch = -(-len(testloader.dataset) / testloader.batch_size)  # e.g. 10000 / 4 (cifar10)
+    print('Start Training...')
+    train_time = time.time()
+    for epoch in range(args.epochs):  # loop over the dataset multiple times
+        train_loss, train_acc, val_loss, val_acc = 0, 0, 0, 0
 
-        # Blur images
-        if not args.normal and epoch < 25:  # not changing images from epoch==25(zero-based index)  
-            inputs = GaussianBlur_images(inputs.cpu(), \
-                                         tuple(args.kernel_size), args.sigma)  
-            inputs = inputs.to(device)
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        train_loss += loss.item()
-        train_acc += (outputs.max(1)[1] == labels).sum().item()        
-        loss.backward()
-        optimizer.step()
-    
-    # record the values in tensorboard
-    writer.add_scalar('loss/train', train_loss / num_trainbatch, epoch + 1)  # avarage loss
-    writer.add_scalar('acc/train', train_acc / len(trainloader.dataset), epoch + 1)  # avarage acc
-        
-    # ===== val mode =====
-    net.eval()
-    with torch.no_grad():
-        for data in testloader:
+        # ===== train mode =====
+        net.train()
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data[0].to(device), data[1].to(device)
-            """
-            if not args.normal:
+
+            # Blur images
+            if not args.normal and epoch < 25:  # not changing images from epoch==25(zero-based index)  
                 inputs = GaussianBlur_images(inputs.cpu(), \
-                                             tuple(args.kernel_size), args.sigma) 
+                                             tuple(args.kernel_size), args.sigma)  
                 inputs = inputs.to(device)
-            """
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
-            val_loss += loss.item()
-            val_acc += (outputs.max(1)[1] == labels).sum().item()
+            train_loss += loss.item()
+            train_acc += (outputs.max(1)[1] == labels).sum().item()        
+            loss.backward()
+            optimizer.step()
+
+        # record the values in tensorboard
+        writer.add_scalar('loss/train', train_loss / num_trainbatch, epoch + 1)  # average loss
+        writer.add_scalar('acc/train', train_acc / len(trainloader.dataset), epoch + 1)  # average acc
+
+        # ===== val mode =====
+        net.eval()
+        with torch.no_grad():
+            for data in testloader:
+                inputs, labels = data[0].to(device), data[1].to(device)
+                """
+                if not args.normal:
+                    inputs = GaussianBlur_images(inputs.cpu(), \
+                                                 tuple(args.kernel_size), args.sigma) 
+                    inputs = inputs.to(device)
+                """
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                val_acc += (outputs.max(1)[1] == labels).sum().item()
+
+        # record the values in tensorboard
+        writer.add_scalar('loss/val', val_loss / num_testbatch, epoch + 1)  # average loss
+        writer.add_scalar('acc/val', val_acc / len(testloader.dataset), epoch + 1)  # average acc
+        
+        # ===== save the model =====
+        # torch.save(net.state_dict(), MODEL_PATH)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': 'alexnet',
+            'state_dict': net.state_dict(),
+            'optimizer': optimizer.state_dict()},
+            MODEL_PATH, epoch + 1)
+
+    print('Finished Training')
+    print("Training time elapsed: {:.4f}mins".format((time.time() - train_time) / 60))
+    print()
     
-    # record the values in tensorboard
-    writer.add_scalar('loss/val', val_loss / num_testbatch, epoch + 1)  # avarage loss
-    writer.add_scalar('acc/val', val_acc / len(testloader.dataset), epoch + 1)  # avarage acc
+    writer.close()  # close tensorboardX writer
+    
 
-print('Finished Training')
-print("Training time elapsed: {:.4f}mins".format((time.time() - train_time) / 60))
-print()
+if __name__ == '__main__':
+    run_time = time.time()
+    main()
+    mins = (time.time() - run_time) / 60
+    hours = mins / 60
+    print("Total run time: {:.4f}mins, {:.4f}hours".format(mins, hours)) 
 
-# save the model
-torch.save(net.state_dict(), MODEL_PATH)
-
-
-# test
-net = AlexNetCifar10().to(device)
-net.load_state_dict(torch.load(MODEL_PATH))
-net.eval()
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(device), data[1].to(device)
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-if not args.normal:
-    print('NOTE: Test images are NOT blurred.')
-    #print('NOTE: Test images are blurred.')
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(device), data[1].to(device)
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(labels.size(0)):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
-
-
-writer.close()  # close tensorboardX writer
