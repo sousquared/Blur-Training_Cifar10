6a0b97a8252414af79cfe0dcaaceecc8708b6dc4
sousquared <sou.yoshiahra.72e@gmail.com>
2020-06-05 07:38:23 +0000

    change the way to save model

--- train/utils.py
+++ train/utils.py
@@ -81,7 +81,7 @@ class ProgressMeter(object):
     
 
 def save_model(state, param_path, epoch):
-    filename=param_path+'model_{:03d}.pth.tar'.format(epoch)
+    filename=param_path+'epoch_{}.pth.tar'.format(epoch)
     torch.save(state, filename)
    
         
